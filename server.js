cfaf4bd49aef92dcb74b9b0ec0b65bc82a407530
R1Slim <pauljaiden5@gmail.com>
2026-04-06 18:29:43 +0000

    fix backend deploy

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.get('/api/health', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`🚀 Server running`);
 });
